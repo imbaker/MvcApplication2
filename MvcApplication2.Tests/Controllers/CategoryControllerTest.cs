312016c1871b3f44517ac97e7dd9626bc965c0c1
Ian Baker <ianbaker@cix.co.uk>
2014-08-06 22:17:07 +0100

    Category_ExecuteIndexAction_ReturnsIndexView

---
+++ MvcApplication2.Tests/Controllers/CategoryControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace MvcApplication2.Tests.Controllers
+{
+    using Moq;
+
+    using MvcApplication2.Controllers;
+    using MvcApplication2.Database.Repositories.Interfaces;
+    using System.Web.Mvc;
+
+    public class CategoryControllerTest
+    {
+        public CategoryControllerTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        [Fact]
+        public void Category_ConstructorAcceptsUnitOfWork()
+        {
+            // Arrange
+            var unitOfWorkStub = Mock.Of<IUnitOfWork>();
+            var result = new CategoryController(unitOfWorkStub);
+        }
+
+        [Fact]
+        public void Category_ExecuteIndexAction_ReturnsIndexView()
+        {
+            // Arrange
+            var unitOfWorkStub = Mock.Of<IUnitOfWork>();
+
+            // Act
+            var result = new CategoryController(unitOfWorkStub).Index() as ViewResult;
+
+            // Assert
+            Assert.Equal("Index", result.ViewName);
+        }
+    }
+}
