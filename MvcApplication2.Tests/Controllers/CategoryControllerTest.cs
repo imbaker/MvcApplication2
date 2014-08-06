8b357a37d88dffcad6375688717ce75a54fdc179
Ian Baker <ianbaker@cix.co.uk>
2014-08-06 22:58:15 +0100

    Category_ExecuteIndexAction_CallsGetAllMethod

--- MvcApplication2.Tests/Controllers/CategoryControllerTest.cs
+++ MvcApplication2.Tests/Controllers/CategoryControllerTest.cs
@@ -2,12 +2,16 @@
 
 namespace MvcApplication2.Tests.Controllers
 {
-    using Moq;
+    using System.Linq;
 
+    using Moq;
     using MvcApplication2.Controllers;
+    using Entities = MvcApplication2.Database.Entities;
     using MvcApplication2.Models;
     using MvcApplication2.Database.Repositories.Interfaces;
     using System.Web.Mvc;
+
+    using MvcApplication2.Tests.Database.Repositories;
 
     public class CategoryControllerTest
     {
@@ -46,6 +50,42 @@ namespace MvcApplication2.Tests.Controllers
 
             // Assert
             Assert.IsType<CategoryIndexViewModel>(model);
+        }
+
+        [Fact]
+        public void Category_ExecuteIndexAction_CallsGetAllMethod()
+        {
+            // Arrange
+            var categoryRepository = new MemoryRepository<Entities.Category>();
+            categoryRepository.Insert(new Entities.Category() { Id = 1, Name = "Category 1" });
+            categoryRepository.Insert(new Entities.Category() { Id = 2, Name = "Category 2" });
+
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            unitOfWorkMock.Setup(m => m.CategoryRepository.GetAll()).Returns(categoryRepository.GetAll());
+
+            // Act
+            var result = new CategoryController(unitOfWorkMock.Object).Index();
+
+            // Assert
+            unitOfWorkMock.Verify(m => m.CategoryRepository.GetAll(), Times.Once);
+        }
+
+        [Fact]
+        public void Repository_GetAll_ReturnsCorrectCountValue()
+        {
+            // Arrange
+            var categoryRepository = new MemoryRepository<Entities.Category>();
+            categoryRepository.Insert(new Entities.Category() { Id = 1, Name = "Category 1" });
+            categoryRepository.Insert(new Entities.Category() { Id = 2, Name = "Category 2" });
+
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            unitOfWorkMock.Setup(m => m.CategoryRepository).Returns(categoryRepository);
+
+            // Act
+            var result = unitOfWorkMock.Object.CategoryRepository.GetAll().Count();
+
+            // Assert
+            Assert.Equal(2, result);
         }
     }
 }
