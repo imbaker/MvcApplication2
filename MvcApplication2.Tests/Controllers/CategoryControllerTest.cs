440460d72eae52599fddb055534789ab90ec5442
Ian Baker <ianbaker@cix.co.uk>
2014-08-20 22:02:47 +0100

    Added display of Category view.

--- MvcApplication2.Tests/Controllers/CategoryControllerTest.cs
+++ MvcApplication2.Tests/Controllers/CategoryControllerTest.cs
@@ -1,76 +1,74 @@
-﻿using Xunit;
-
-namespace MvcApplication2.Tests.Controllers
+﻿namespace MvcApplication2.Tests.Controllers
 {
     using System.Linq;
 
     using Moq;
+
+    using MvcApplication2.App_Start;
     using MvcApplication2.Controllers;
     using Entities = MvcApplication2.Database.Entities;
     using MvcApplication2.Models;
     using MvcApplication2.Database.Repositories.Interfaces;
     using System.Web.Mvc;
 
+    using NUnit.Framework;
+
     using MvcApplication2.Tests.Database.Repositories;
 
     public class CategoryControllerTest
     {
-        private readonly IUnitOfWork UnitOfWorkStub;
+        private IUnitOfWork UnitOfWorkMock;
 
-        public CategoryControllerTest()
+        [SetUp]
+        public void CategoryControllerTest_SetUp()
         {
-            //
-            // TODO: Add constructor logic here
-            //
-            UnitOfWorkStub = Mock.Of<IUnitOfWork>();
-        }
+            AutomapperConfig.Register();
 
-        [Fact]
-        public void Category_ConstructorAcceptsUnitOfWork()
-        {
-            var result = new CategoryController(this.UnitOfWorkStub);
-        }
-
-        [Fact]
-        public void Category_ExecuteIndexAction_ReturnsIndexView()
-        {
-            // Act
-            var result = new CategoryController(this.UnitOfWorkStub).Index() as ViewResult;
-
-            // Assert
-            Assert.Equal("Index", result.ViewName);
-        }
-
-        [Fact]
-        public void Category_ExecuteIndexAction_ViewContainsCategoryIndexViewModel()
-        {
-            // Act
-            var result = new CategoryController(this.UnitOfWorkStub).Index() as ViewResult;
-            var model = result.Model;
-
-            // Assert
-            Assert.IsType<CategoryIndexViewModel>(model);
-        }
-
-        [Fact]
-        public void Category_ExecuteIndexAction_CallsGetAllMethod()
-        {
-            // Arrange
             var categoryRepository = new MemoryRepository<Entities.Category>();
             categoryRepository.Insert(new Entities.Category() { Id = 1, Name = "Category 1" });
             categoryRepository.Insert(new Entities.Category() { Id = 2, Name = "Category 2" });
 
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            unitOfWorkMock.Setup(m => m.CategoryRepository.GetAll()).Returns(categoryRepository.GetAll());
-
-            // Act
-            var result = new CategoryController(unitOfWorkMock.Object).Index();
-
-            // Assert
-            unitOfWorkMock.Verify(m => m.CategoryRepository.GetAll(), Times.Once);
+            this.UnitOfWorkMock = Mock.Of<IUnitOfWork>(m => m.CategoryRepository.GetAll() == categoryRepository.GetAll());
         }
 
-        [Fact]
+        [Test]
+        public void Category_ConstructorAcceptsUnitOfWork()
+        {
+            var result = new CategoryController(this.UnitOfWorkMock);
+        }
+
+        [Test]
+        public void Category_ExecuteIndexAction_ReturnsIndexView()
+        {
+            // Act
+            var result = new CategoryController(this.UnitOfWorkMock).Index() as ViewResult;
+
+            // Assert
+            Assert.That(result.ViewName, Is.EqualTo("Index").IgnoreCase);
+        }
+
+        [Test]
+        public void Category_ExecuteIndexAction_ViewContainsCategoryIndexViewModel()
+        {
+            // Act
+            var result = new CategoryController(this.UnitOfWorkMock).Index() as ViewResult;
+            var model = result.Model;
+
+            // Assert
+            Assert.That(model, Is.TypeOf<CategoryIndexViewModel>());
+        }
+
+        [Test]
+        public void Category_ExecuteIndexAction_CallsGetAllMethod()
+        {
+            // Act
+            var result = new CategoryController(UnitOfWorkMock).Index();
+
+            // Assert
+            Mock.Get(UnitOfWorkMock).Verify(m => m.CategoryRepository.GetAll(), Times.Once);
+        }
+
+        [Test]
         public void Repository_GetAll_ReturnsCorrectCountValue()
         {
             // Arrange
@@ -82,10 +80,10 @@ namespace MvcApplication2.Tests.Controllers
             unitOfWorkMock.Setup(m => m.CategoryRepository).Returns(categoryRepository);
 
             // Act
-            var result = unitOfWorkMock.Object.CategoryRepository.GetAll().Count();
+            var numberOfItems = unitOfWorkMock.Object.CategoryRepository.GetAll().Count();
 
             // Assert
-            Assert.Equal(2, result);
+            Assert.That(numberOfItems, Is.EqualTo(2));
         }
     }
 }
